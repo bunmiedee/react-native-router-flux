3db47ae00005c3fd23e8f5a0b9b602425d0bf3ca
Olubunmi Adedire <bunmiedee@gmail.com>
2019-11-27 15:00:00 +0000

    tabs
    wip

--- src/createTabNavigatorHOC.js
+++ src/createTabNavigatorHOC.js
@@ -1,8 +1,27 @@
 /* @flow */
 
-import { createNavigator, TabRouter } from 'react-navigation';
+import React from 'react';
+import { View } from 'react-native';
+import { createNavigator, createNavigationContainer, TabRouter } from 'react-navigation';
+
+const TabsWithChild = props => (
+  <View>
+    {props.children}
+  </View>
+);
 
 export default NavigationView => (routeConfigs, navigationConfig = {}) => {
+  console.log(':::NavigationView');
   const router = TabRouter(routeConfigs, navigationConfig);
-  return createNavigator(NavigationView, router, navigationConfig);
+  return (
+    <TabsWithChild
+      router={router}>
+      {
+        createNavigationContainer(
+          createNavigator(NavigationView, router, navigationConfig)
+        )
+      }
+    </TabsWithChild>
+  )
+  //return createNavigationContainer(createNavigator(NavigationView, router, navigationConfig));
 };
